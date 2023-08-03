cf8af553b4c7af9757ddd17c71b3455e22058ff0
Yago Silva <yago.mdk284@gmail.com>
2023-08-03 12:59:45 -0300

    Include Validations

--- src/main/java/com/ead/authuser/controllers/AuthenticationController.java
+++ src/main/java/com/ead/authuser/controllers/AuthenticationController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,7 +32,7 @@ public class AuthenticationController {
 
     @PostMapping("/signup")
     public ResponseEntity<Object> registerUser(
-            @RequestBody @JsonView(UserDto.UserView.RegistationPost.class) UserDto userDto) {
+            @RequestBody @Validated(UserDto.UserView.RegistationPost.class) @JsonView(UserDto.UserView.RegistationPost.class) UserDto userDto) {
 
         if (userService.existsByUsername(userDto.getUsername()))
             return ResponseEntity.status(HttpStatus.CONFLICT).body("ERROR: Username is Already Taken!");
--- src/main/java/com/ead/authuser/dtos/UserDto.java
+++ src/main/java/com/ead/authuser/dtos/UserDto.java
@@ -3,6 +3,15 @@ package com.ead.authuser.dtos;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonView;
 import java.util.UUID;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.br.CPF;
+import org.springframework.format.annotation.NumberFormat;
+import org.springframework.validation.annotation.Validated;
+
 import lombok.Data;
 
 @Data
@@ -26,27 +35,39 @@ public class UserDto {
 
   private UUID userId;
 
+  @NotBlank(groups = UserView.RegistationPost.class)
+  @Size(min = 4, max = 60)
   @JsonView(UserView.RegistationPost.class)
   private String username;
 
+  @NotBlank(groups = UserView.RegistationPost.class)
+  @Email
   @JsonView(UserView.RegistationPost.class)
   private String email;
 
+  @NotBlank(groups = { UserView.RegistationPost.class, UserView.PasswordPut.class })
   @JsonView({ UserView.RegistationPost.class, UserView.PasswordPut.class })
   private String password;
 
+  @NotBlank(groups = UserView.PasswordPut.class)
   @JsonView(UserView.PasswordPut.class)
   private String oldPassword;
 
+  @NotBlank(groups = { UserView.RegistationPost.class, UserView.UserPut.class })
+  @Size(min = 4, max = 60)
   @JsonView({ UserView.RegistationPost.class, UserView.UserPut.class })
   private String fullName;
 
+  @NotBlank(groups = { UserView.RegistationPost.class, UserView.UserPut.class })
   @JsonView({ UserView.RegistationPost.class, UserView.UserPut.class })
   private String phoneNumber;
 
+  @NotBlank(groups = { UserView.RegistationPost.class, UserView.UserPut.class })
+  @CPF
   @JsonView({ UserView.RegistationPost.class, UserView.UserPut.class })
   private String cpf;
 
+  @NotBlank(groups = UserView.ImagePut.class)
   @JsonView(UserView.ImagePut.class)
   private String imageUrl;
 }
